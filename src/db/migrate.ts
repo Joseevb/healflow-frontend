b9c38c7d4cef929cdf0958f00c92d7d959ad8347
Jose Vasquez <joseevb@protonmail.com>
2026-02-15 17:42:33 +0100

    feat: added log to migrations step

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -3,6 +3,11 @@ import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 import { drizzle } from "drizzle-orm/bun-sqlite";
 import { Database } from "bun:sqlite";
 
-const sqlite = new Database(process.env.DB_FILE_NAME);
-const db = drizzle(sqlite);
-migrate(db, { migrationsFolder: "./drizzle" });
+try {
+  const sqlite = new Database(process.env.DB_FILE_NAME);
+  const db = drizzle(sqlite);
+  migrate(db, { migrationsFolder: "./drizzle" });
+  console.log("migrations completed");
+} catch (error) {
+  console.error("Migration failed:", error);
+}
