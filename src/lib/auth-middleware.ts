dccabd27c50e1611af30a2c0aeab4a0b5685314a
Jose Vasquez <joseevb@protonmail.com>
2026-02-21 21:36:29 +0100

    feat: updated auth middleware

--- src/lib/auth-middleware.ts
+++ src/lib/auth-middleware.ts
@@ -1,20 +1,16 @@
 import { createMiddleware } from "@tanstack/react-start";
 import { getRequestHeaders } from "@tanstack/react-start/server";
-import { getSession } from "@/lib/auth-client";
 import { auth } from "@/lib/auth";
 import { attempt } from "@/lib/attempt";
 
 export const authMiddleware = createMiddleware().server(async ({ next }) => {
   const headers = getRequestHeaders();
 
-  const { data: sessionRes, error: sessionErr } = await attempt(() =>
-    getSession({
-      fetchOptions: {
-        headers: headers,
-      },
+  const { data: session } = await attempt(() =>
+    auth.api.getSession({
+      headers: headers,
     }),
   );
-  const session = sessionErr ? null : sessionRes.data;
 
   const { data: jwtRes, error: jwtErr } = await attempt(() =>
     auth.api.getToken({ headers: headers }),
